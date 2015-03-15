975def1de7fa999e5372a8f7ce5993ec4336ae96
DaviTeodoro <davi.a.teodoro@gmail.com>
2015-03-15 18:19:27 -0300

    First commit

--- answers/NUMPERF.py
+++ answers/NUMPERF.py
@@ -1,1 +1,11 @@
+np = int(input('Say a number: '))
+som = 0
+for i in range(1,np):
+	if np%i == 0:
+		print (i),
+		som += i 
 
+if som == np:
+	print('It is a perfect number!')
+else:
+	print ('It is not a perfect number')
